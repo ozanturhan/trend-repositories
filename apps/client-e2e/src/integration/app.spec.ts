ba2224ed0bfd63b48270dd2f3921d3b7ca245173
Ozan Turhan <moturhan@ebay.com>
2022-03-02 14:08:06 +0300

    fix e2e tests

--- apps/client-e2e/src/integration/app.spec.ts
+++ apps/client-e2e/src/integration/app.spec.ts
@@ -16,8 +16,6 @@ describe('client', () => {
       method: 'POST',
       url: '/graphql',
     }).as('graphql');
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(5 * 1000);
   });
 
   it('should display application name', () => {
@@ -25,24 +23,23 @@ describe('client', () => {
   });
 
   it('should filter repositories by language ', () => {
+    cy.wait('@graphql');
     const languages = selectLanguage();
     languages.select('PHP').should('have.value', 'PHP');
-    cy.wait('@graphql');
   });
 
   it('should filter repositories by date ', () => {
+    cy.wait('@graphql');
     const languages = selectDate();
     languages.select('Last month').should('have.value', 'last_month');
-    cy.wait('@graphql');
   });
 
   it('should visit next and prev page ', () => {
-    const next = nextPage();
-
-    next.trigger('click');
     cy.wait('@graphql');
-    cy.wait(5 * 1000);
+    const next = nextPage();
+    next.trigger('click');
 
+    cy.wait('@graphql');
     const prev = previousPage();
     prev.trigger('click');
   });
